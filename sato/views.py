a38060bf25770aa6ba0744226768e184d80ba86b
kazuki0259 <ksw2070302@stu.o-hara.ac.jp>
2021-09-17 06:33:18 +0000

    views 変更

--- sato/views.py
+++ sato/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
 
-def index(request):
-    pass
+class IndexView(generic.TemplateView):
+    template_name = 'blog/index.html'
